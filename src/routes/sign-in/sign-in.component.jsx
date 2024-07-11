4d8205fa9f47aff4d6c2b3fb7a6ce8755f2d56d5
youssef AIT ALI <youssef.aitali@outlook.com>
2024-07-11 16:05:13 +0100

    Adding sign in with redirect to complete

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,18 +1,42 @@
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
+
 import {
+  auth,
   signInWithGooglePopup,
+  signInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
+  /*  useEffect(async () => {
+    const response = await getRedirectResult(auth);
+    console.log(response);
+  }, []); */
+  const fetUserAuth = async () => {
+    const response = await getRedirectResult(auth);
+    console.log(response);
+  };
+  useEffect(() => {
+    fetUserAuth();
+  }, []);
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
+  };
+  const logGoogleRedirectUser = async () => {
+    const { user } = await signInWithGoogleRedirect();
+    console.log({ user });
   };
 
   return (
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      <button onClick={signInWithGoogleRedirect}>
+        Sign in with Google Redirect
+      </button>
     </div>
   );
 };
